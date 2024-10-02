5075d89fb7d521bf30fe9d48c8766886be0a0acd
ddwx3399 <pcj3399@hotmail.com>
2024-10-02 23:22:59 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -43,7 +43,7 @@ async def login(username, password, panel):
         await page.type('#id_username', username)
         await page.type('#id_password', password)
 
-        login_button = await page.querySelector('#submit')
+        login_button = await page.querySelector('#login_check')
         if login_button:
             await login_button.click()
         else:
